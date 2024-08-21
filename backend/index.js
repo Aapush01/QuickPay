6c3a869aafae2eb8f98a5daf43e894a2c82619cf
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-21 23:40:25 +0530

    backend codes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mainRouter = require("./routes/index");
+const cors = require("cors");
+const app = express();
 
 require('dotenv').config();
-const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", mainRouter);
 
 app.get('/', (req, res) => {
     res.send('Backend here!');
